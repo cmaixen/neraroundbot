fd9648cb748fb64e9578cb5173063b11d524de49
arknsensei <asengonul@gmail.com>
2016-12-01 21:36:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,14 +16,14 @@ else {
 /**
  * Auth Control
  */
-function (msg) {
+bot.on('message', function (msg) {
   if (msg.chat.id==274298910) {
         return;
       } else {
       bot.sendMessage(msg.chat.id, msg.from.first_name + "You are NOT Authorized to use this BOT");
-        return;
+      bot.getme;
   }
- };
+ });
 
 
 /**
