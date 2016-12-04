e8806f3ecb4c9659ba6e36f2e67d572f54e6eddd
arknsensei <asengonul@gmail.com>
2016-12-04 20:46:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,42 +55,6 @@ if (process.env.NODE_ENV === 'production') {
    */
    bot.onText(/\/show/, function (msg, match) {
      var list = FirebaseManager.showList(bot, msg.chat.id);
-  /*    
-      var items = Object.keys(list);
-      items.forEach(function(item) {
-         bot.sendMessage(msg.chat.id, item + '=' + list[item]);
-      });
-     bot.sendMessage(msg.chat.id, 'deneme\n');
-     //bot.sendMessage(msg.chat.id, '1' + list[0]);
-     
-    
-           var outputListArr1 = list.split("####"),
-               count1 = 0,
-               countforlisting1 = 0,
-               listnumber1 = 0,   
-               outputStr1 = '',
-               outputListStr1 = '';
-     
-      outputStr1 = 'Like & Comment RECENT \nCWD with @ \nGO!!! \n\n' ;
-     
-      for(var i=0; i<outputListArr1.length; i+=1){
-        if (i==0) {
-          outputStr1 += outputListArr1[i];
-          bot.sendMessage(msg.chat.id, outputStr1 +'\n1-' + i);
-          //FirebaseManager.prototype.sleep (1000);
-        } else {
-          if (i==outputListArr1.length-1) {
-            outputStr1 = outputListArr1[i] + (count1+1) + ' participants';
-            bot.sendMessage(msg.chat.id, outputStr1 +'\n2-' + i);
-            //FirebaseManager.prototype.sleep (1000);
-           } else { 
-             outputStr1 = outputListArr1[i];
-             bot.sendMessage(msg.chat.id, outputStr1 +'\n3-' + i);
-             //FirebaseManager.prototype.sleep (1000);
-          }
-        }      
-      }
-*/
    });
 
   /**
