6239d43ac0e8f9f07d314eb7a7184c1ad30e4375
arknsensei <asengonul@gmail.com>
2016-12-05 19:22:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ var Bot = require('node-telegram-bot-api');
 var bot;
 
 var FirebaseManager = require('./FirebaseManager.js');
+var outhControl = require('./outhControl.js');
 
 var kontrolchatid;
 var AdminList = "274298910###262889034";
@@ -18,18 +19,22 @@ if (process.env.NODE_ENV === 'production') {
      var message = "Chat Id = "+msg.chat.id + "\nYour ID = " +  msg.from.id;
      bot.sendMessage(msg.chat.id, message);
    } else {
-     bot.sendMessage(msg.chat.id, 'You are not authorized to use me! Please contact my master!');
+     bot.sendMessage(msg.chat.id, 'You are not authorized to use me! Please contact my master!\n/help');
    }
  });
 
 //match /create [list name] 
  bot.onText(/\/start (.+)/, function (msg, match) {
-   if(AdminList.indexOf(msg.from.id) >= 0) {
-     var message = "Drop @'s \nRound started!!";
-     FirebaseManager.createList(msg.chat.id, match[1], msg.from, 1);
-     bot.sendMessage(msg.chat.id, message);
-   } else {
-     bot.sendMessage(msg.chat.id, 'You are not authorized to use me! Please contact my masters!');
+   if(outhControl) {
+     if(AdminList.indexOf(msg.from.id) >= 0) {
+       var message = "Drop @'s \nRound started!!";
+       FirebaseManager.createList(msg.chat.id, match[1], msg.from, 1);
+       bot.sendMessage(msg.chat.id, message);
+     } else {
+       bot.sendMessage(msg.chat.id, 'You are not authorized to use me! Please contact my masters!\n/help');
+     }
+    } else {
+     bot.sendMessage(msg.chat.id, 'I dont work for this group. Please contact my masters if you want me to host your rounds too!\n/help');
    }
   });
 
@@ -37,49 +42,68 @@ if (process.env.NODE_ENV === 'production') {
   * matches @ -- Account ekle
   */
   bot.onText(/@/, function (msg, match) {
-    if(msg.text.indexOf('@') == 0) {
-      //bot.sendMessage(msg.chat.id, msg.from.first_name + " " + msg.text + " Added!");
-      FirebaseManager.managerGuests(bot, msg.chat.id, msg.text, 'add');
-    }
+   if(outhControl) {
+      if(msg.text.indexOf('@') == 0) {
+        //bot.sendMessage(msg.chat.id, msg.from.first_name + " " + msg.text + " Added!");
+        FirebaseManager.managerGuests(bot, msg.chat.id, msg.text, 'add');
+      }
+    } else {
+     bot.sendMessage(msg.chat.id, 'I dont work for this group. Please contact my masters if you want me to host your rounds too!\n/help');
+   }
   });
 
   /**
     * matches D -- Done account
    */
    bot.onText(/D (.+)/, function (msg, match) {
+   if(outhControl) {
      //bot.sendMessage(msg.chat.id, msg.from.first_name + " " + match[1].split(" ")[0] + " Done!");
      FirebaseManager.managerGuests(bot, msg.chat.id, match[1].split(" ")[0], 'remove');
+    } else {
+     bot.sendMessage(msg.chat.id, 'I dont work for this group. Please contact my masters if you want me to host your rounds too!\n/help');
+   }
    });
 
   /**
    * matches /remove -- Listeden cikart
    */
    bot.onText(/Remove! (.+)/, function (msg, match) {
+   if(outhControl) {
      //bot.sendMessage(msg.chat.id, msg.from.first_name + " " + match[1] + " removed from the round!");
      FirebaseManager.managerGuests(bot, msg.chat.id, match[1], 'remove');
-     //FirebaseManager.managerParticipants(bot, msg.chat.id, msg.from.first_name, msg.from.last_name, 'remove');
+    } else {
+     bot.sendMessage(msg.chat.id, 'I dont work for this group. Please contact my masters if you want me to host your rounds too!\n/help');
+   }
    });
 
   /**
    * matches /showList
    */
    bot.onText(/\/show/, function (msg, match) {
-   if(AdminList.indexOf(msg.from.id) >= 0) {
-       var list = FirebaseManager.showList(bot, msg.chat.id);
-     } else {
-       bot.sendMessage(msg.chat.id, 'You are not authorized to use me! Please contact my master!');
-     }
+   if(outhControl) {
+     if(AdminList.indexOf(msg.from.id) >= 0) {
+         var list = FirebaseManager.showList(bot, msg.chat.id);
+       } else {
+         bot.sendMessage(msg.chat.id, 'You are not authorized to use me! Please contact my master!\n/help');
+       }
+    } else {
+     bot.sendMessage(msg.chat.id, 'I dont work for this group. Please contact my masters if you want me to host your rounds too!\n/help');
+   }
    });
 
   /**
    * matches /showList
    */
-   bot.onText(/\/check/, function (msg, match) {
-     if(AdminList.indexOf(msg.from.id) >= 0) {
-       var list = FirebaseManager.showListCheck(bot, msg.chat.id);
-     } else {
-       bot.sendMessage(msg.chat.id, 'You are not authorized to use me! Please contact my master!');
-     }
+   bot.onText(/\/check/, function (msg, match) {     
+   if(outhControl) {
+       if(AdminList.indexOf(msg.from.id) >= 0) {
+         var list = FirebaseManager.showListCheck(bot, msg.chat.id);
+       } else {
+         bot.sendMessage(msg.chat.id, 'You are not authorized to use me! Please contact my master!\n/help');
+       }
+    } else {
+     bot.sendMessage(msg.chat.id, 'I dont work for this group. Please contact my masters if you want me to host your rounds too!');
+   }
    });
 
    /**
