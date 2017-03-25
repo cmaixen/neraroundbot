1a9e23fcf1cb34a5d71efb738cc797079361d05f
arknsensei <asengonul@gmail.com>
2017-03-26 00:54:48 +0300

    aa

--- nextr.js
+++ nextr.js
@@ -3,20 +3,32 @@ var nextr = function(roundlist) {
 var moment = require('moment');
 var getroundtime = require('./getRoundTime.js');
 var output = "";
+var timecheck = [];
 
 var currentTime = moment();
 
       var getrounds = getroundtime(roundlist);
       var rtimeItems = Object.keys(getrounds);
           rtimeItems.forEach(function(item) {
-
-          var roundrelease = moment(getrounds[item], 'HH:mm');
-
-          output = currentTime.format('HH:mm');
-
+          timecheck.push(moment(getrounds[item], 'HH:mm'));
     });
 
+/*
+    for(var i=0; i<timecheck.length; i+=1){
+        if (i == timecheck.length) {
 
+        } 
+        else {
+            if (currentTime > timecheck[i] && currentTime > timecheck[i+1]) {
+
+            } 
+        }
+
+    }
+*/
+var roundrelease = moment('10:00', 'HH:mm');
+output = moment(roundrelease).tonow 
+//output = currentTime.format('HH:mm');
 
     return output;
 
